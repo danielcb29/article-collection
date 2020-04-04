ce0c77ac8c1f6d44f47e6c821744553a2210bd32
danielcb29 <danielc.web@gmail.com>
2020-04-04 17:02:39 -0500

    Creating the base of the project

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    React: 'writable',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+    'react/prop-types': [0]
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ["./"]
+      }
+    },
+    react: {
+      pragma: "React",
+      version: "16.9.0"
+    }
+  },
+  parser: "babel-eslint",
+};
